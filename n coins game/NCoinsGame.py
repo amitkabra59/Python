242fa02c7684bbd7d613a6937bac71dfec59d745
Amit Kabra <amitkabra59@gmail.com>
2018-12-05 21:03:29 +0100

    Code added

--- n coins game/NCoinsGame.py
+++ n coins game/NCoinsGame.py
@@ -1,1 +1,14 @@
 
+def winner(n, A, B):
+	if((n-1)%6 == 0):
+		print B, " is the winner"
+	else:
+		print A, " is the winner"
+		
+def main():
+	n = input("enter the number of coins :")
+	first = input("enter the name of the player who starts the game :")
+	second = input("enter the name of the 2nd player :")
+	winner(n, first, second)
+		
+main()
